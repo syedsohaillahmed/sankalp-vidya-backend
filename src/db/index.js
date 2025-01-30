55eae29e8b4d9dd4d462705f846e0aa31fb10aab
syedsohaillahmed <syedsohaillahmed19@gmail.com>
2025-01-31 01:39:40 +0530

    added basic express setup, cookie parser and cors setup completed

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB =  async ()=>{
-    try {
-       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`mongodb connected !! DB HOST ${process.env.PORT}` )
-    } catch (error) {
-        console.log("mongodb connection error", error)
-        process.exit(1)
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`mongodb connected !! DB HOST ${process.env.PORT}`);
+  } catch (error) {
+    console.log("mongodb connection error", error);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // require("dotenv").config({path:"./.env"})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:"./.env"
-})
+  path: "./.env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("server is running");
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection error");
+  });
